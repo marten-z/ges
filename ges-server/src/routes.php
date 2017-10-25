51ee382ed7df0075ebe80896fb04e4947efa2f54
Marten <martenhzijlstra@gmail.com>
2017-10-25 21:45:54 +0200

    Enable CORS

--- ges-server/src/routes.php
+++ ges-server/src/routes.php
@@ -3,6 +3,20 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+// Enabling CORS
+
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+    
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+    ->withHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
+
 // Routes
 
 $app->get('/', function (Request $request, Response $response, array $args) {
